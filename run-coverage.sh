76c8c4eb100f255be9d6546eed30ebbe3112a50c
farnkbolero <frank@frankbolero.com>
2026-01-05 23:12:12 +0100

    formatting

--- run-coverage.sh
+++ run-coverage.sh
@@ -7,6 +7,7 @@ dotnet test --collect:"XPlat Code Coverage" --results-directory ./TestResults
 echo "Generating HTML coverage report..."
 reportgenerator -reports:"TestResults/**/coverage.cobertura.xml" -targetdir:"TestResults/CoverageReport" -reporttypes:Html
 
+
 echo ""
 echo "Coverage report generated successfully!"
 echo "Open the report: TestResults/CoverageReport/index.html"
